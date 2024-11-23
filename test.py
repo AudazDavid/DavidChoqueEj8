acf96c380f7017315612bad60cd54097f1e5ef2d
AudazDavid <164928165+AudazDavid@users.noreply.github.com>
2024-11-22 21:29:48 -0300

    Update test.py
    Modificación 02 moviendo la funcion y la condicional dentro de TestSum

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import unittest
 
 class TestSum(unittest.TestCase):
-def test_sum(self):
-  self.assertEqual(sum(1, 2), 3)
+  def test_sum(self):
+    self.assertEqual(sum(1, 2), 3)
 
-if __name__ == '__main__':
-  unittest.main()
+  if __name__ == '__main__':
+    unittest.main()
 
