5a939a4afcec605e6a45f5d5668f377f92eedb6e
AudazDavid <164928165+AudazDavid@users.noreply.github.com>
2024-11-22 21:24:11 -0300

    Update test.py
    Modificando el archivo test.py (la linea 5 y 8 no estaban dentro de la defincion de la función "def" y la condicional "if" respectivamente)

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import unittest
 
 class TestSum(unittest.TestCase):
 def test_sum(self):
-self.assertEqual(sum(1, 2), 3)
+  self.assertEqual(sum(1, 2), 3)
 
 if __name__ == '__main__':
-unittest.main()
+  unittest.main()
 
